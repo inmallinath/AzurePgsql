d021acdbb49911ad6fc16ef8d2e2393f0cd6d93b
Mallinath Nagaraj <mallinathin@gmail.com>
2017-06-14 18:25:17 -0700

    create project files

---
+++ Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzurePgSql.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
